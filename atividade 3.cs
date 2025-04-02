08157d1002759c6d73d6fc6660eac5789104623c
ssorie <141286721+ssorie@users.noreply.github.com>
2025-04-02 20:36:14 -0300

    Add files via upload

---
+++ atividade 3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade_2_aula_dia_02_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            //1. inicio
+            //2. var inicio: caracter, preço, prestação, total:real
+            //3. leia nome
+            //4. leia preço
+            //5. total = preço - preço * 0.10 || total = preço - preço * 10/100
+            //6. escreva o total
+            //7. total = preço + preço * 0.02
+            //8. escreva o total
+            //9. total = preço + preço * 0.05
+            //10. escreva o total
+            //11. fim.
+        {
+            string inicio;
+            double preço, total;
+        }
+    }
+}
